4d06a2a8b31465d04e2820380479fa174a89eecd
Bartosz Stoliński <barf@krej.net>
2020-10-29 23:00:55 +0100

    Witcher arm wrestling logic

---
+++ Assets/_Lab/Witcher/WitcherLogic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class WitcherLogic : MonoBehaviour {
+    public WitcherVisualisation ui;
+    public bool reduceBarPermanently = false;
+    public float barReductionPerSecond = 5;
+
+
+    void Update() {
+        ui.PointX = ui.MouseX;
+        if (IsMouseOverBar())
+            ui.BarX++;
+        else
+            ui.BarX--;
+
+        if(reduceBarPermanently)
+            ui.BarWidth -= barReductionPerSecond * Time.deltaTime;
+        else
+            ui.BarWidth = (1000 - ui.BarX) / 4;
+    }
+
+    bool IsMouseOverBar() {
+        return Mathf.Abs(ui.BarX - ui.MouseX) < ui.BarWidth / 2;
+    }
+}
